0d7f55996a6656d9e1f931694e3d758505975cf8
RappyTV <rappytv@rappytv.com>
2023-04-16 10:49:14 +0200

    Created isConnectedToServer()

--- core/src/main/java/com/rappytv/opsucht/util/Util.java
+++ core/src/main/java/com/rappytv/opsucht/util/Util.java
@@ -1,13 +1,21 @@
 package com.rappytv.opsucht.util;
 
+import com.rappytv.opsucht.OPSuchtAddon;
 import net.labymod.api.Laby;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.gui.icon.Icon;
+import net.labymod.api.labynet.models.ServerGroup;
 import net.labymod.api.notification.Notification;
 import net.labymod.api.notification.Notification.Type;
 import net.labymod.api.util.I18n;
+import java.util.Optional;
 
 public class Util {
+
+    public static boolean isConnectedToOpSucht() {
+        Optional<ServerGroup> currentServer = Laby.labyAPI().labyNetController().getCurrentServer();
+        return currentServer.isPresent() && currentServer.get().getDirectIp().equals(OPSuchtAddon.ip);
+    }
 
     public static String getTranslation(String key, Object... args) {
         if(!I18n.has(key))
