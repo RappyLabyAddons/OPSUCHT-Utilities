3eaf43023cbc06067fa1c6c0ebbdcd0a7ee6d667
RappyTV <rappytv@rappytv.com>
2023-04-16 06:26:15 +0200

    Added a friend request option to the context menu

---
+++ core/src/main/java/com/rappytv/opsucht/context/FriendRequestContext.java
@@ -0,0 +1,41 @@
+package com.rappytv.opsucht.context;
+
+import com.rappytv.opsucht.OPSuchtAddon;
+import com.rappytv.opsucht.OPSuchtConfig;
+import com.rappytv.opsucht.util.Util;
+import net.labymod.api.Laby;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.entity.player.Player;
+import net.labymod.api.client.entity.player.interaction.BulletPoint;
+import net.labymod.api.client.gui.icon.Icon;
+
+public class FriendRequestContext implements BulletPoint {
+
+    private final OPSuchtConfig config;
+
+    public FriendRequestContext(OPSuchtAddon addon) {
+        this.config = addon.configuration();
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.text(Util.getTranslation("opsucht.context.friendRequest"));
+    }
+
+    @Override
+    public Icon getIcon() {
+        return null;
+    }
+
+    @Override
+    public void execute(Player player) {
+        Laby.labyAPI().minecraft().executeNextTick(
+            () -> Laby.labyAPI().minecraft().chatExecutor().chat("/freund hinzufügen " + player.getName())
+        );
+    }
+
+    @Override
+    public boolean isVisible(Player playerInfo) {
+        return config.clanInviteContext().get();
+    }
+}
