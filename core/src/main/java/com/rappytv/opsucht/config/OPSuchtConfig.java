a3f6dc9fd29123ba5d139d94caf8bad6c74b462e
RappyTV <rappytv@rappytv.com>
2023-10-19 10:14:27 +0200

    Add update info to rank option

--- core/src/main/java/com/rappytv/opsucht/config/OPSuchtConfig.java
+++ core/src/main/java/com/rappytv/opsucht/config/OPSuchtConfig.java
@@ -5,6 +5,7 @@ import com.rappytv.opsucht.config.subconfig.DiscordRPCSubconfig;
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
+import net.labymod.api.configuration.loader.annotation.IntroducedIn;
 import net.labymod.api.configuration.loader.annotation.SpriteSlot;
 import net.labymod.api.configuration.loader.annotation.SpriteTexture;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
@@ -20,6 +21,8 @@ public class OPSuchtConfig extends AddonConfig {
     @SpriteSlot(size = 32)
     @SwitchSetting
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
+    @IntroducedIn(namespace = "opsucht", value = "1.4.0")
+    @SwitchSetting
     private final ConfigProperty<ServerRank> rank = new ConfigProperty<>(ServerRank.PLAYER);
     @SpriteSlot(size = 32, x = 1)
     private final DiscordRPCSubconfig discordRPCSubconfig = new DiscordRPCSubconfig();
