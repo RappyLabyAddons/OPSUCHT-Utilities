103bc6447cece5750f186e2245a46419a3a11f71
RappyTV <rappytv@rappytv.com>
2023-12-07 00:07:16 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ labyMod {
         author = "RappyTV"
         description = "Adds some ingame utilities for OPSUCHT.net | Not affiliated with Interwebmedia GmbH"
         minecraftVersion = "1.19<*"
-        version = System.getenv().getOrDefault("VERSION", "1.1.8")
+        version = System.getenv().getOrDefault("VERSION", "1.1.9")
     }
 
     minecraft {
