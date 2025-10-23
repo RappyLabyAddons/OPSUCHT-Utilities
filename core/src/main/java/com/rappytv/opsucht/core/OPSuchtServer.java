a4cbdfdb36ff9050ec8c62775c7680eff026d449
RappyTV <rappytv@rappytv.com>
2025-10-23 10:37:00 +0200

    fix: Add one second delay to /fly command Fixes #43

--- core/src/main/java/com/rappytv/opsucht/core/OPSuchtServer.java
+++ core/src/main/java/com/rappytv/opsucht/core/OPSuchtServer.java
@@ -3,15 +3,22 @@ package com.rappytv.opsucht.core;
 import net.labymod.api.Laby;
 import net.labymod.api.client.network.server.AbstractServer;
 import net.labymod.api.event.Phase;
+import net.labymod.api.util.concurrent.task.Task;
+
+import java.util.concurrent.TimeUnit;
 
 public class OPSuchtServer extends AbstractServer {
 
     private final OPSuchtAddon addon;
+    private final Task autoFlyTask;
     private boolean connected;
 
     public OPSuchtServer(OPSuchtAddon addon) {
         super("opsucht");
         this.addon = addon;
+        this.autoFlyTask = Task.builder(() ->
+            Laby.references().chatExecutor().chat("/fly", false)
+        ).delay(1, TimeUnit.SECONDS).build();
         this.connected = false;
     }
 
@@ -19,9 +26,7 @@ public class OPSuchtServer extends AbstractServer {
     public void loginOrSwitch(LoginPhase phase) {
         if(phase == LoginPhase.LOGIN) this.connected = true;
         else if(phase == LoginPhase.SWITCH && this.addon.configuration().autoFly().get()) {
-            Laby.labyAPI().minecraft().executeNextTick(() ->
-                Laby.references().chatExecutor().chat("/fly", false)
-            );
+            this.autoFlyTask.execute();
         }
 
         Laby.labyAPI().minecraft().executeNextTick(() ->
