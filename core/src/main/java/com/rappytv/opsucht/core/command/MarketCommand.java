64369fcdb9784bad241e3d7c0452f679099e639c
RappyTV <rappytv@rappytv.com>
2026-02-21 14:32:30 +0100

    fix: Subcommands return wrong error when not connected

--- core/src/main/java/com/rappytv/opsucht/core/command/MarketCommand.java
+++ core/src/main/java/com/rappytv/opsucht/core/command/MarketCommand.java
@@ -60,7 +60,11 @@ public class MarketCommand extends Command {
         @Override
         public boolean execute(String prefix, String[] arguments) {
             if(!this.addon.server().isConnected()) {
-                return false;
+                this.displayMessage(OPSuchtAddon.prefix().append(Component.translatable(
+                    this.getTranslationKey("notConnected"),
+                    NamedTextColor.RED
+                )));
+                return true;
             }
             ClientPlayer player = Laby.labyAPI().minecraft().getClientPlayer();
             if(player == null) {
@@ -148,7 +152,11 @@ public class MarketCommand extends Command {
         @Override
         public boolean execute(String prefix, String[] arguments) {
             if(!this.addon.server().isConnected()) {
-                return false;
+                this.displayMessage(OPSuchtAddon.prefix().append(Component.translatable(
+                    this.getTranslationKey("notConnected"),
+                    NamedTextColor.RED
+                )));
+                return true;
             }
             ClientPlayer player = Laby.labyAPI().minecraft().getClientPlayer();
             if(player == null) {
